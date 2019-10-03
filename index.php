43635659c0ad1ed20c30e3b244afe9e9e84d8351
Neo Anoman <neoanoman@gmail.com>
2019-10-03 16:44:56 +0530

    added_code

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
               <div class="card-body" style= " font-size: 1.4em">
                 <div class="tab-content">
                   <div class="tab-pane fade show active" id="panel1" role="tabpanel">
-                    <form class="form-signin">
+                    <form class="form-signin" action="#">
                       <div class="form-label-group">
                         <label for="inputEmail">Email address</label>
                         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
@@ -93,7 +93,7 @@
                         <input type="checkbox" class="custom-control-input" id="customCheck1">
                         <label class="custom-control-label" for="customCheck1">Remember password</label>
                       </div>
-                      <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                      <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" onclick="<?php handleCache(); ?>" >Sign in</button>
                       <hr class="my-4">
                     </form>
                   </div>
@@ -150,6 +150,11 @@
         </div>
       </div>
     </div>
+    <?php
+    function handleCache() {
+      echo "<h1>I will handle Cache</h1>";
+    }
+     ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
